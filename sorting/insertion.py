bc0ee856edd41f24d5fe677165800d27a77e9518
Natalie Gomez <ngomez9@asu.edu>
2017-10-27 13:55:50 -0700

    modified insertion sort

--- sorting/insertion.py
+++ sorting/insertion.py
@@ -5,12 +5,11 @@ def insertion(numbers):
 
 		j = i-1
 		while j >= 0 and key < numbers[j]:
-			# TODO: if element is greater than the key,
 			# move ahead of their current position
             if numbers[j] > key:
                 numbers[j-i] = numbers[j]
 			
-            j--
+            j = j -1
 
 		numbers[j+1] = key
 
